af1f6ce6b30e8fab3edc45d533fa72812cf202e9
zheyuanf <91371741+zheyuanf@users.noreply.github.com>
2025-01-30 15:40:53 +0800

    add user service (#5)
    * add user app
    
    * add user app service register
    
    ---------
    
    Co-authored-by: 符哲源 <fuzheyuan@fuzheyuandeMacBook-Pro.local>

---
+++ app/user/biz/model/user.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	Base
+	Email          string `gorm:"unique"`
+	PasswordHashed string
+}
+
+func (u User) TableName() string {
+	return "user"
+}
+
+// 根据邮箱查询用户
+func GetByEmail(db *gorm.DB, ctx context.Context, email string) (user *User, err error) {
+	err = db.WithContext(ctx).Model(&User{}).Where(&User{Email: email}).First(&user).Error
+	return
+}
+
+// 创建用户
+func Create(db *gorm.DB, ctx context.Context, user *User) error {
+	return db.WithContext(ctx).Create(user).Error
+}
--- app/user/biz/service/register.go
+++ app/user/biz/service/register.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
+	"errors"
+
+	"github.com/zheyuanf/ecommerce-tiktok/app/user/biz/dal/mysql"
+	"github.com/zheyuanf/ecommerce-tiktok/app/user/biz/model"
 	user "github.com/zheyuanf/ecommerce-tiktok/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterService struct {
@@ -15,6 +20,28 @@ func NewRegisterService(ctx context.Context) *RegisterService {
 // Run create note info
 func (s *RegisterService) Run(req *user.RegisterReq) (resp *user.RegisterResp, err error) {
 	// Finish your business logic.
+	// 1. 参数校验
+	if req.Email == "" || req.Password == "" || req.ConfirmPassword == "" {
+		return nil, errors.New("email and password are required")
+	}
+	if req.Password != req.ConfirmPassword {
+		return nil, errors.New("password and confirm password do not match")
+	}
 
-	return
+	// 2. 计算密码的hash值
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return
+	}
+
+	// 3. 创建用户
+	newUser := &model.User{
+		Email:          req.Email,
+		PasswordHashed: string(passwordHashed),
+	}
+	if err = model.Create(mysql.DB, s.ctx, newUser); err != nil {
+		return
+	}
+
+	return &user.RegisterResp{UserId: int32(newUser.ID)}, nil
 }
