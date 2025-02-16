d8fbeb6bb4da8ad6e4afda02d2f99bb7820e7cc4
heilili <2020961976@qq.com>
2025-02-16 09:32:49 +0800

    add circuit breaker

--- app/frontend/go.mod
+++ app/frontend/go.mod
@@ -71,6 +71,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kitex-contrib/config-consul v0.1.4 // indirect
 	github.com/kitex-contrib/registry-consul v0.1.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -1,6 +1,11 @@
 package rpc
 
 import (
+	"context"
+	"github.com/cloudwego/kitex/pkg/circuitbreak"
+	"github.com/cloudwego/kitex/pkg/fallback"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/zheyuanf/ecommerce-tiktok/rpc_gen/kitex_gen/product"
 	"sync"
 
 	"github.com/cloudwego/kitex/client"
@@ -42,8 +47,39 @@ func InitClient() {
 }
 
 func initProductClient() {
-	// TODO: 熔断和降级等逻辑待补充
-	ProductClient, err = productcatalogservice.NewClient("product", commonSuite)
+	// 熔断配置
+	cbs := circuitbreak.NewCBSuite(func(ri rpcinfo.RPCInfo) string {
+		return circuitbreak.RPCInfo2Key(ri)
+	})
+	cbs.UpdateServiceCBConfig("frontend/product/GetProduct",
+		circuitbreak.CBConfig{Enable: true, ErrRate: 0.5, MinSample: 2})
+	ProductClient, err = productcatalogservice.NewClient("product", commonSuite,
+		client.WithCircuitBreaker(cbs), client.WithFallback(
+			fallback.NewFallbackPolicy(
+				fallback.UnwrapHelper(
+					func(ctx context.Context, req, resp interface{}, err error) (fbResp interface{}, fbErr error) {
+						if err == nil {
+							return resp, nil
+						}
+						// 若为ListProducts方法，则返回一个默认的商品，防止界面为空
+						methodName := rpcinfo.GetRPCInfo(ctx).To().Method()
+						if methodName != "ListProducts" {
+							return resp, nil
+						}
+						return &product.ListProductsResp{
+							Products: []*product.Product{
+								{
+									Price:       6.6,
+									Id:          3,
+									Picture:     "/static/image/t-shirt.jpeg",
+									Name:        "T-shirt",
+									Description: "static",
+								},
+							},
+						}, nil
+					}),
+			),
+		))
 	frontendutils.MustHandleError(err)
 }
 
