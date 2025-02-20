b7ff99ba740545f76c9d7b45252631a9a3050946
heilili <2020961976@qq.com>
2025-02-20 15:17:00 +0800

    add email-send

--- app/email/infra/notify/email.go
+++ app/email/infra/notify/email.go
@@ -1,14 +1,27 @@
 package notify
 
 import (
-	"github.com/kr/pretty"
+	"crypto/tls"
 	"github.com/zheyuanf/ecommerce-tiktok/rpc_gen/kitex_gen/email"
+	"gopkg.in/gomail.v2"
+	"log"
 )
 
 type NoopEmail struct{}
 
 func (e *NoopEmail) Send(req *email.EmailReq) error {
-	pretty.Printf("%v\n", req)
+	// 发送邮件
+	m := gomail.NewMessage()
+	m.SetHeader("From", req.From)
+	m.SetHeader("To", req.To)
+	m.SetHeader("Subject", req.Subject)
+	m.SetBody(req.ContentType, req.Content)
+	d := gomail.NewDialer("smtp.163.com", 465, "abcde706@163.com", "JYafnACEvDNRiu9g")
+	// 跳过验证
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(m); err != nil {
+		log.Fatal(err)
+	}
 	return nil
 }
 
